5bf017babd7025dba7326b5373593c0a819f9bf6
joaop1325@gmail.com <joaop1325@gmail.com@8e333b71-6908-85ba-0947-8a8802aedcb8>
2013-03-14 16:04:43 +0000

    Flare Oclusion added...

---
+++ Assets/Scripts/camera/flareOclusion.js
@@ -0,0 +1,37 @@
+//this script controls if all flares are showing or not
+#pragma strict
+
+var maxDistance : float;
+var mainCamera : GameObject;
+var flareObject : GameObject[];
+
+function Start () {
+
+	mainCamera = Camera.main.gameObject;
+	
+
+
+}
+
+function Update () {
+
+	if (Vector3.Distance(mainCamera.transform.position, transform.position) <= maxDistance)
+	{
+	
+		for (var flare : GameObject in flareObject)
+		{
+			var lightObj : Light = flare.GetComponent(Light);
+			lightObj.enabled = true;
+		}
+	
+	}
+	else
+	{
+		for (var flare : GameObject in flareObject)
+		{
+			var lightObj1 : Light = flare.GetComponent(Light);
+			lightObj1.enabled = true;
+		}
+	}
+
+}
