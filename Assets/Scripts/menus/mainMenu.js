6b867da573ef1cb2343bd729dcf6544c85edd98c
joaop1325@gmail.com <joaop1325@gmail.com@8e333b71-6908-85ba-0947-8a8802aedcb8>
2013-07-08 15:01:19 +0000

    continued working on the main menu

--- Assets/Scripts/menus/mainMenu.js
+++ Assets/Scripts/menus/mainMenu.js
@@ -1,7 +1,11 @@
 #pragma strict
 
 //control vars
-var mainBool : boolean = true;
+var isMain : boolean = true;
+var isPlay : boolean = false;
+var isSave : boolean = false;
+var isOption : boolean = false;
+var isCredits : boolean = false;
 
 
 
@@ -19,7 +23,6 @@ class Button {
 }
 
 //main menu
-var gradientMain : Texture2D;
 var MainWidth : int;
 var MainHeight : int;
 var MainX :int;
@@ -27,8 +30,38 @@ var MainY : int;
 
 
 var PlayButton : Button;
+var LoadButton : Button;
 var OptionsButton : Button;
 var CreditsButton : Button;
+
+//version label
+var VerCoodX : int;
+var VerCoodY : int;
+var VerWidth : int;
+var VerHeight : int;
+var VerText : String;
+var VerStyle : GUIStyle;
+
+//Play menu
+var PlayWidth : int;
+var PlayHeight : int;
+
+var playBG : Texture2D;
+var playBGW : int;
+var playBGH : int;
+
+//play title label
+var playTitleX : int;
+var playTitleY : int;
+var playTitleW : int;
+var playTitleH : int;
+var playTitleStyle : GUIStyle;
+
+//difficulty buttons
+var difEasy : Button;
+var difNormal : Button;
+var difHard : Button;
+var difReturn : Button;
 
 
 
@@ -45,9 +78,16 @@ function OnGUI() {
 	//Draw Background
 	GUI.DrawTexture(Rect(0,0, Screen.width, Screen.height), bgImg, bgMode);
 	
-	if(mainBool) {
+	//Draw Version Text
+	GUI.Label(Rect(Screen.width - VerCoodX, VerCoodY, VerWidth, VerHeight), VerText, VerStyle);
+	
+	
+	if(isMain) {
 		MainMenu();		
-	}	
+	}
+	else if (isPlay) {
+		PlayMenu();
+	}
 
 	//Draw exit button
 	if(GUI.Button(Rect(ExitButton.CoodX, Screen.height - ExitButton.CoodY, ExitButton.width, ExitButton.height), "Quit", ExitButton.Style)) {
@@ -63,7 +103,14 @@ function MainMenu() {
 	//Create area
 	GUILayout.BeginArea(Rect(Screen.width - MainX, Screen.height - MainY, MainWidth, MainHeight));
 		//Draw Play Button
-		GUI.Button(Rect(PlayButton.CoodX, PlayButton.CoodY, PlayButton.width, PlayButton.height), "Play Game", PlayButton.Style);
+		if(GUI.Button(Rect(PlayButton.CoodX, PlayButton.CoodY, PlayButton.width, PlayButton.height), "Play Game", PlayButton.Style)) {
+			isMain = false;
+			isPlay = true;
+		}
+		
+		//Draw Load Game Button
+		GUI.Button(Rect(LoadButton.CoodX, LoadButton.CoodY, LoadButton.width, LoadButton.height), "Load Game", LoadButton.Style);
+		
 		
 		//Draw Options Button
 		GUI.Button(Rect(OptionsButton.CoodX, OptionsButton.CoodY, OptionsButton.width, OptionsButton.height), "Options", OptionsButton.Style);
@@ -74,5 +121,34 @@ function MainMenu() {
 	GUILayout.EndArea();
 	
 	
+
+}
+
+function PlayMenu () {
+	//Create area
+	GUILayout.BeginArea(Rect(Screen.width/2 - PlayWidth/2, Screen.height/2 - PlayHeight/2, PlayWidth, PlayHeight));
+		//Draw Background
+		GUI.DrawTexture(Rect(0,0, playBGW, playBGH), playBG);
+		
+		//Write title label
+		GUI.Label(Rect(playTitleX, playTitleY, playTitleW, playTitleH), "Select Dificulty", playTitleStyle);
+		
+		//Easy button
+		GUI.Button(Rect(difEasy.CoodX, difEasy.CoodY, difEasy.width, difEasy.height), "Easy", difEasy.Style);
+		
+		//Normal button
+		GUI.Button(Rect(difNormal.CoodX, difNormal.CoodY, difNormal.width, difNormal.height), "Normal", difNormal.Style);
+		
+		//Hard button
+		GUI.Button(Rect(difHard.CoodX, difHard.CoodY, difHard.width, difHard.height), "Hard", difHard.Style);
+		
+		//Return Main Menu button
+		if(GUI.Button(Rect(difReturn.CoodX, difReturn.CoodY, difReturn.width, difReturn.height), "Main Menu", difReturn.Style)) {
+			isPlay = false;
+			isMain = true;
+		}
+		
+	GUILayout.EndArea();
+
 
 }
