8cd1547cd7266bffab21d1c38b7f81be412824a4
joaop1325@gmail.com <joaop1325@gmail.com@8e333b71-6908-85ba-0947-8a8802aedcb8>
2013-09-13 15:53:49 +0000

    Changed some more stuff, began work on Map Info system

---
+++ Assets/Scripts/SaveGame/MapInfo.js
@@ -0,0 +1,50 @@
+﻿#pragma strict
+
+class PlanetInfo { //this class stores all planet information necessary for the map
+	var isEnabled : boolean;
+	var name : String;
+	var scene : String;
+	var description : String;
+	var image : Texture2D;
+	var cood : PlanetCood;
+	var defenseForce : SaveShip[];
+	
+	class PlanetCood {
+		
+		var x : int;
+		var y : int;
+	
+	}
+
+}
+
+var planets : PlanetInfo[];
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnGUI () {
+	//check if the map is up on the player ship
+	//first get the save game script to access the FindPlayerShip function
+	var saveScr : SaveGame = GameObject.FindGameObjectWithTag("SaveGame").GetComponent(SaveGame);
+	var playerShip : GameObject = saveScr.FindPlayerShip(); //new we get said ship
+	var playerGui : guiScript = playerShip.GetComponent(guiScript); //new we get the gui script from said ship
+	var isMap : boolean = playerGui.isMap; //now get the value
+	
+	//now we check the isMap value
+	
+	if(isMap) { //if its true, prepare to draw the map
+	
+	
+	}
+	
+	
+	
+	
+}
+
