c22a1b31b9404040ab655e46122b9be255497c01
jooborrego4@gmail.com <jooborrego4@gmail.com@8e333b71-6908-85ba-0947-8a8802aedcb8>
2013-11-27 16:55:03 +0000

    Begun work on messaging system

---
+++ Assets/Scripts/menus/ShowMessage.js
@@ -0,0 +1,35 @@
+﻿import System.Collections.Generic;
+#pragma strict
+
+///<summary> This class contains the message definition </summary>
+class Message {
+
+	var message : String;
+	
+	public function Message(message : String) {
+	
+		this.message = message;
+	
+	}
+	
+	public function getMessage() : String {
+	
+		return message;
+	
+	}
+
+}
+
+var messages : List.< Message>;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function AddMessage(message : String) {
+	messages.Add(new Message(message));
+}
