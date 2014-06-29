b32a5c5b0d626b9e29e5b9843c5530c0d5ab4cf1
joaop1325@gmail.com <joaop1325@gmail.com@8e333b71-6908-85ba-0947-8a8802aedcb8>
2014-06-29 20:34:13 +0000

    Fixed station bug where they couldn't find correct weapon. Re-implemented screenshot button on F11.

--- Assets/Scripts/camera/screenshot.cs
+++ Assets/Scripts/camera/screenshot.cs
@@ -5,46 +5,46 @@ using UnityEngine;
 using System.Collections;
 using System.IO;
 
-public class screenshot : MonoBehaviour {
+public class screenshot : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// LateUpdate is called once per frame
-	void LateUpdate () {
-		
-		if(!Application.isWebPlayer)
+		// Use this for initialization
+		void Start ()
 		{
-			if(Input.GetKeyDown (KeyCode.F11))
-			{
-				//create game directory
-				string direc = Application.dataPath;
-				
-				
-				direc = direc + "/../Screenshots/";
-				
-				
-				if(!System.IO.Directory.Exists(@direc))
-				{
-					System.IO.Directory.CreateDirectory(@direc);	
-				}
-				
-				//count files already present
-				System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(direc);
-		    	int count = dir.GetFiles().Length;
-				
-				//extend the name
-				direc = direc + "Screenshot" + count.ToString () + ".png";
-				
-				//save the screenshot
-				Application.CaptureScreenshot (direc);
-				
-			}
+	
 		}
+	
+		// LateUpdate is called once per frame
+		void LateUpdate ()
+		{
 		
-	}
+				if (!Application.isWebPlayer) {
+						if (Input.GetKeyDown (KeyCode.F11)) {
+								//create game directory
+								string direc = Application.dataPath;
+				
+				
+								direc = direc + "/Screenshots/";
+				
+				
+								if (!System.IO.Directory.Exists (@direc)) {
+										System.IO.Directory.CreateDirectory (@direc);	
+								}
+				
+								//count files already present
+								System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo (direc);
+								int count = dir.GetFiles ().Length;
+				
+								//extend the name
+								direc = direc + "Screenshot" + count.ToString () + ".png";
+				
+								//save the screenshot
+								Application.CaptureScreenshot (direc);
+				
+						}
+				}
+		
+		}
 	
 	
 	
