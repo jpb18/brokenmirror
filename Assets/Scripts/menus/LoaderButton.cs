e775b32ceabd454fa5dc24d2e9a9611fd329f7bc
joaop1325@gmail.com <joaop1325@gmail.com@8e333b71-6908-85ba-0947-8a8802aedcb8>
2014-07-04 17:29:54 +0000

    Added LoaderButton.cs Created Credits Button on main menu.

---
+++ Assets/Scripts/menus/LoaderButton.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoaderButton : MonoBehaviour
+{
+
+		public Rect buttonPos;
+		public string label;
+		public string scene;
+		public bool isLeft = true;
+		public bool isTop = true;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		void OnGUI ()
+		{
+
+				if (GUI.Button (getRect (), label)) {
+						Application.LoadLevel (scene);
+				}
+
+		}
+
+		private Rect getRect ()
+		{
+
+				Rect rect = new Rect ();
+
+				if (isLeft) {
+						rect.x = buttonPos.x;
+				} else {
+						rect.x = Screen.width - buttonPos.x;
+				}
+
+				if (isTop) {
+						rect.y = buttonPos.y;
+				} else {
+						rect.y = Screen.height - buttonPos.y;
+				}
+
+				rect.width = buttonPos.width;
+				rect.height = buttonPos.height;
+
+				return rect;
+
+		}
+
+}
