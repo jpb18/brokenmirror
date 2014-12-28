4d01993b24c600ac5bb8cfea06eaacbc8826108c
jooborrego4@gmail.com <jooborrego4@gmail.com@8e333b71-6908-85ba-0947-8a8802aedcb8>
2014-12-28 23:24:40 +0000

    Added Raptor model and prepared ship for use (which is ready for it). Added Galor model.

---
+++ Assets/Models/Ships/Cardassian/Galor/readme.txt
@@ -0,0 +1,52 @@
+####################################################
+                    GALOR CLASS
+####################################################
+
+[General Informations:]
+
+Ship(s):	Cardassian Galor Class
+Filename:	galor_class_v1.zip
+Version:	1.0
+Date:		22 April 2002
+Author:		Nine of Nine
+E-mail:		nineof9@freemail.it
+
+[Ship Details]
+
+Lods:		high, medium, low
+Textures:	4 high, 4 medium, 4 low
+Illumination:	yes
+Specular Maps:	4 high, 4 medium
+Hardpoints:	yes
+
+[Installation]
+unzip this zip in a temporary folder and then copy data and scripts directories in you activision/bridge commander folder. if asked overwrite the old files and all files will be updated. 
+
+[Credits]
+-Meshe and texture by Nine of Nine
+-Hardpoints and scripting by USS Phoenix
+-Beta testing by Starforce
+
+[Thanks]
+to USS Phoenix who scripted the ship (without him i couldn't have released this ship)
+to Starforce who beta test this ship
+to Pneumonic81 who help me very much (directly and indirectly with his awesome ships)
+to everyone has replyed in my galor thread at bcn network
+
+[Other Informations]
+ship created in 3d studio max v3.1 and converted with SDK plug-in
+
+[Copyright]
+
+THIS MODEL(S) IS NOT MADE, DISTRIBUTED, OR SUPPORTED BY Activision TM & (C) INTERPLAY & PARAMOUNT PICTURES.
+
+Star Trek, Star Trek: The Next Generation, Star Trek: Deep Space Nine, Star Trek: Voyager
+and related properties are Registered Trademarks of Paramount Pictures
+registered in the United States Patent and Trademark Office.
+All original designs are copyright © Paramount Pictures.
+No infringement of Paramount's copyrights is intended.
+
+[Distribution]
+
+All files in this zip are for personal use only and cannot be bought or sold. They also cannot be modified without author's permission.
+They can be uploaded in any site but credits MUST be given.
--- Assets/Standard Assets/Scripts/NewInputManager/InputManager.cs
+++ Assets/Standard Assets/Scripts/NewInputManager/InputManager.cs
@@ -47,6 +47,121 @@ namespace NewInputManager
             return a.Press;
         }
 
+        public void CreateAxis(string name, string description, KeyCode key)
+        {
+            if (this.axis.ContainsKey(name))
+            {
+                Debug.LogWarning("Axis named " + name + " already exists.");
+                return;
+            }
+
+            Axis axis = new KeyAxis(name, description, key);
+
+            this.axis.Add(name, axis);
+
+
+        }
+
+        public void CreateAxis(string name, string description, MouseButton button)
+        {
+            if (this.axis.ContainsKey(name))
+            {
+                Debug.LogWarning("Axis named " + name + " already exists.");
+                return;
+            }
+
+            Axis axis = new MouseAxis(name, description, button);
+            this.axis.Add(name, axis);
+
+        }
+
+        public void CreateAxis(string name, string description)
+        {
+            if (this.axis.ContainsKey(name))
+            {
+                Debug.LogWarning("Axis named " + name + " already exists.");
+                return;
+            }
+
+            Axis axis = new ScrollAxis(name, description);
+            this.axis.Add(name, axis);
+
+        }
+        /// <summary>
+        /// This edits the axis to be a KeyCode axis
+        /// </summary>
+        /// <param name="name">name of the axis</param>
+        /// <param name="key">keycode for the axis</param>
+        public void EditAxis(string name, KeyCode key)
+        {
+            Axis axis = null;
+            if (this.axis.TryGetValue(name, out axis))
+            {
+                Axis newAxis = new KeyAxis(name, axis.Description, key);
+                this.axis[name] = newAxis;
+            }
+            else
+            {
+                Debug.LogWarning("There's no axis named " + name + ".");
+            }
+
+            
+
+        }
+
+        /// <summary>
+        /// This edits the axis to be a mouse button axis
+        /// </summary>
+        /// <param name="name">name of the axis</param>
+        /// <param name="button">keycode for the axis</param>
+        public void EditAxis(string name, MouseButton button)
+        {
+            Axis axis = null;
+            if (this.axis.TryGetValue(name, out axis))
+            {
+                Axis newAxis = new MouseAxis(name, axis.Description, button);
+                this.axis[name] = newAxis;
+            }
+            else
+            {
+                Debug.LogWarning("There's no axis named " + name + ".");
+            }
+        }
+
+        /// <summary>
+        /// This edits the axis to be a scrollwheel axis
+        /// </summary>
+        /// <param name="name">name of the axis</param>
+        public void EditAxis(string name)
+        {
+            Axis axis = null;
+            if (this.axis.TryGetValue(name, out axis))
+            {
+                Axis newAxis = new ScrollAxis(name, axis.Description);
+                this.axis[name] = newAxis;
+            }
+            else
+            {
+                Debug.LogWarning("There's no axis named " + name + ".");
+            }
+        }
+
+        public void RemoveAxis(string name)
+        {
+            if (!this.axis.Remove(name))
+            {
+                Debug.LogWarning("There's no axis named " + name + ".");
+            }
+        } 
+
+        public Dictionary<string, Axis>.Enumerator Enumerator
+        {
+            get
+            {
+                return axis.GetEnumerator();
+            }
+        }
+
     }
 
 }
