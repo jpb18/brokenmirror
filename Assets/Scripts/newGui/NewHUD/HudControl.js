368b5cddf0e1d2abf3e62a1b4cc21faaeac0a5c4
jpb18 <joao_pedro_borrego@hotmail.com>
2015-07-07 19:23:28 +0100

    Finished helm hud component.

--- Assets/Scripts/newGui/NewHUD/HudControl.js
+++ Assets/Scripts/newGui/NewHUD/HudControl.js
@@ -17,6 +17,11 @@ private var movement : shipMovement;
 private var properties : shipProperties;
 
 
+//repeater button stuff
+private var isIncreasingSpeed : boolean = false;
+private var isDecreasingSpeed : boolean = false;
+
+
 function Start () {
 
 }
@@ -60,7 +65,54 @@ function ShowHud() {
 }
 
 ///<summary>This updates the ship speed</summary>
-function UpdateShipSpeed() {
+private function UpdateShipSpeed() {
 	forwardSpeedBar.value = movement.GetForwardPercentage();
 	backwardSpeedBar.value = movement.GetBackwardsPercentage();
+	
+	if(this.isIncreasingSpeed) {
+		this.IncreaseShipSpeed();
+	} else if (this.isDecreasingSpeed) {
+		this.DecreaseShipSpeed();
+	}
+	
+}
+
+///<summary>This sets the control for speed increase</summary>
+function PressSpeedIncrease() {
+	this.isIncreasingSpeed = true;
+}
+
+///<summary>This unsets the control for speed increase</summary>
+function ReleaseSpeedIncrease() {
+	this.isIncreasingSpeed = false;
+}
+
+///<summary>This sets the control for speed decrease</summary>
+function PressSpeedDecrease() {
+	this.isDecreasingSpeed = true;
+}
+
+///<summary>This unsets the control for speed decrease</summary>
+function ReleaseSpeedDecrease() {
+	this.isDecreasingSpeed = false;
+}
+
+
+///<summary>This allows the HUD button to increase the ships speed. If speed it's at maximum, or a Full Stop Order has been issued, it'll do nothing</summary>
+private function IncreaseShipSpeed() {
+	if(!movement.isAtMax() && !movement.isChanging)
+		movement.increaseSpeed();
+}
+
+///<summary>This allows the HUD Button to reduce the ships speed. If Speed it's at minimum, or a Full Stop order has been issued, it'll do nothing</summary>
+private function DecreaseShipSpeed() {
+	if(!movement.isAtMin() && !movement.isChanging)
+		movement.decreaseSpeed();
+}
+
+///<summary>This makes the ship go full stop. If it's stopped, or the speed is changing, it'll ignore</summary>
+function FullStop() {
+	if(!movement.isStop() && !movement.isChanging) {		
+				movement.fullStop();	  
+	} 
 }
